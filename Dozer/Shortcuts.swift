b153441d29181be9db86cad86442528e7bea023b
xgitqa <134912634+xgitqa@users.noreply.github.com>
2026-06-02 14:29:04 +0200

    Replace MASShortcut with KeyboardShortcuts, add release workflow, update README and appcast
    - Replace unmaintained MASShortcut (fails Xcode 15) with sindresorhus/KeyboardShortcuts 2.x
    - Bump deployment target to macOS 12.0 (required by KeyboardShortcuts 2.x; arm64 Macs all support 12+)
    - Remove MASShortcutView from General.xib and bridging header; add RecorderCocoa programmatically
    - Add xcodebuild archive + zip artifact upload to build.yml for every push
    - Add release.yml workflow: builds universal binary on v* tags and publishes GitHub Release
    - Fix appcast.xml to point to xgitqa/Dozer releases instead of upstream
    - Update README: Apple Silicon fork context, install instructions, signing note

--- Dozer/Shortcuts.swift
+++ Dozer/Shortcuts.swift
@@ -2,9 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#ifndef bridging_header_h
-#define bridging_header_h
+import KeyboardShortcuts
 
-#import <Cocoa/Cocoa.h>
-
-#endif
+extension KeyboardShortcuts.Name {
+    static let toggleDozerIcons = Self("toggleDozerIcons")
+}
